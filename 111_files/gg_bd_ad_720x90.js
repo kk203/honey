3132fdd9f32858b6c451d748a4f9d11d0c3276e9
kk <2678880678@qq.com>
2017-03-12 22:03:42 +0800

    lovefoever

---
+++ 111_files/gg_bd_ad_720x90.js
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>娴极鍒嗕韩缃?404椤甸潰</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="The free 404-Event Iphone web template, Andriod web template, Smartphone web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<link href="404/css/style.css"/*tpa=http://www.hunlipic.com/404/css/style.css*/ rel="stylesheet" type="text/css" media="all" />
+</head>
+<body>
+<div class="wrap">
+<div class="main">
+	<div class="banner">
+		<img src="404/images/banner.jpg"/*tpa=http://www.hunlipic.com/404/images/banner.jpg*/ alt="" />
+	</div>
+	<div class="text">
+		<h1>浜诧紝404浜嗭紝浣犲湪鎼炰粈涔堬紒缃戝潃閮借緭閿欎簡锛佹墦灞庝綘锛?</h1>
+		<p>鏄ュぉ鏉ヤ簡,涓?缇ゅぇ闆佹鍚戝寳椋?,涓?浼氬効鎺掓垚B瀛楀瀷,涓?浼氬効鎺掓垚T瀛楀瀷.. </p>
+	</div>
+</div>
+<div class="footer">
+	<p>Design by <a href="javascript:if(confirm('http://www.hunlipic.com/  \n\n该文件无法用 Teleport Ultra 下载, 因为 它是一个域或路径外部被设置为它的启始地址的地址。  \n\n你想在服务器上打开它?'))window.location='http://www.hunlipic.com/'" tppabs="http://www.hunlipic.com/">娴极鍒嗕韩缃?</a></p>
+</div>
+</div>
+<embed style="FILTER: blue()" width="0" height="0" src="../music.163.com/outchain/player-type=2&id=86363&auto=1&height=66" tppabs="http://music.163.com/outchain/player?type=2&id=86363&auto=1&height=66" autostart="false"></embed>
+</body>
+</html>
+
